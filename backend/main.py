e3d6dbcb5baea80deb971802cebe4c18824bcb4c
Loc Huynh <huynhloc2003@gmail.com>
2026-06-13 01:44:25 -0700

    Opti again

--- backend/main.py
+++ backend/main.py
@@ -184,7 +184,8 @@ def verify(
 
         if image is None:
             return unsupported_file_response(file)
-        
+        image.thumbnail((900, 900))
+
         ocr_result = run_ocr_with_rotation(image)
 
         print("OCR SUCCESS")
